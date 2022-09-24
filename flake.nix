7eb7d0c676511b0f0f80705029643e0a38028969
Roman Salmin <Roman.Salmin@gmail.com>
2022-09-24 17:08:36 +0300

    fixed executable name in flake

--- flake.nix
+++ flake.nix
@@ -7,8 +7,8 @@
   outputs = { self, nixpkgs }:
     let pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in  {
-    packages.x86_64-linux.playsdl = pkgs.stdenv.mkDerivation {
-          pname = "playsdl";
+    packages.x86_64-linux.sdlplay = pkgs.stdenv.mkDerivation {
+          pname = "sdlplay";
           version = "0.1";
           src = self;
 
@@ -17,8 +17,6 @@
           buildInputs = with pkgs; [ SDL2 plan9port ];
     };
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.playsdl;
-
-
+    packages.x86_64-linux.default = self.packages.x86_64-linux.sdlplay;
   };
 }
