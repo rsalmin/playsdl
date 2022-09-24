18768537cdaf1375a4406a7d51d88c7a94b7dd17
Roman Salmin <Roman.Salmin@gmail.com>
2022-09-24 14:14:45 +0300

    added executable to flake

--- Makefile
+++ Makefile
@@ -1,12 +1,21 @@
-
 SDL2_CFLAGS = $(shell sdl2-config --cflags)
-
 CXX_FLAGS = $(SDL2_CFLAGS)
-
 LD_FLAGS = -lSDL2
 
-main: main.cpp
+all: sdlplay
+
+sdlplay: main.cpp
 	$(CXX) -o $@ $(CXX_FLAGS) $^ $(LD_FLAGS)
 
 clean:
-	-rm -f main
+	-rm -f sdlplay
+
+install: all
+	mkdir -p ${PREFIX}/bin
+	cp -f sdlplay ${PREFIX}/bin
+	chmod 755 ${PREFIX}/bin/sdlplay
+
+uninstall:
+	rm -f ${PREFIX}/bin/sdlplay
+
+.PHONY: all clean install uninstall
--- flake.nix
+++ flake.nix
@@ -7,6 +7,17 @@
   outputs = { self, nixpkgs }:
     let pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in  {
+    packages.x86_64-linux.playsdl = pkgs.stdenv.mkDerivation {
+          pname = "playsdl";
+          version = "0.1";
+          src = self;
+
+          installFlags = [ "PREFIX=$(out)" ];
+
+          buildInputs = with pkgs; [ SDL2 ];
+    };
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.playsdl;
 
     devShell.x86_64-linux = pkgs.stdenv.mkDerivation {
         name = "ok";
