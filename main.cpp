2815012ee446e381e75dff6b71c8ca0a716f5de0
Roman Salmin <Roman.Salmin@gmail.com>
2022-09-24 20:02:06 +0300

    load bmp in sdl example

--- main.cpp
+++ main.cpp
@@ -1,23 +1,44 @@
 #include <iostream>
-
+#include <memory>
 #include <SDL.h>
 
-int main()
+template<>
+class std::default_delete<SDL_Window>
+{
+public:
+    void operator()(SDL_Window *ptr) const
+    {
+        SDL_DestroyWindow( ptr );
+    }
+};
+
+template<>
+class std::default_delete<SDL_Surface>
+{
+public:
+    void operator()(SDL_Surface *ptr) const
+    {
+        SDL_FreeSurface( ptr );
+    }
+};
+
+
+
+
+std::unique_ptr<SDL_Window> init()
 {
     //Screen dimension constants
     const int SCREEN_WIDTH = 640;
     const int SCREEN_HEIGHT = 480;
 
-    SDL_Window* window = NULL;
-    SDL_Surface* screenSurface = NULL;
-
     if (SDL_Init( SDL_INIT_VIDEO ) < 0 )
     {
         std::cerr << "SDL could not intialize! SDL_error: " << SDL_GetError() << std::endl;
+        return nullptr;
     }
     else
     {
-        window = SDL_CreateWindow( "SDL Tutorial",
+        SDL_Window* window = SDL_CreateWindow( "SDL Tutorial",
             SDL_WINDOWPOS_UNDEFINED,
             SDL_WINDOWPOS_UNDEFINED,
             SCREEN_WIDTH,
@@ -26,24 +47,47 @@ int main()
         if ( NULL == window )
         {
             std::cerr << "Window could not be created! SDL_error: " << SDL_GetError() << std::endl;
+            return nullptr;
         }
-        else
-        {
-            screenSurface = SDL_GetWindowSurface( window );
-            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-            SDL_UpdateWindowSurface( window );
+        return std::unique_ptr<SDL_Window>(window);
+    }
+}
 
-            SDL_Event e;
-            bool quit = false;
-            while ( !quit )
+std::unique_ptr<SDL_Surface> loadMedia()
+{
+  SDL_Surface* surface = SDL_LoadBMP("media/peace.bmp");
+  if (NULL == surface)
+  {
+      std::cerr << "Unable to load image! SDL_error: " << SDL_GetError() << std::endl;
+      return nullptr;
+  }
+  return std::unique_ptr<SDL_Surface>(surface);
+}
+
+int main()
+{
+    auto window = init();
+    if ( !window )
+        return -1;
+
+    auto peaceImage = loadMedia();
+    if ( !peaceImage )
+        return -1;
+
+    SDL_Surface* screenSurface = SDL_GetWindowSurface( window.get() );
+    SDL_BlitSurface( peaceImage.get(), NULL, screenSurface, NULL );
+    //SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+    SDL_UpdateWindowSurface( window.get() );
+
+    SDL_Event e;
+    bool quit = false;
+    while ( !quit )
             {
                while ( SDL_PollEvent( &e ) )
                {
                   quit = SDL_QUIT == e.type ;
                }
             }
-        }
-        SDL_DestroyWindow(  window );
-        SDL_Quit();
-    }
+
+    SDL_Quit();
 }
